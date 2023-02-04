24ffa1980ebd607b0852994e178ee7f47a8490c7
Marlon <marlonhcooper2@gmail.com>
2023-02-04 18:18:08 -0500

    Make completely functional

--- script.js
+++ script.js
@@ -1,34 +1,64 @@
 /* 
-
+The goal of the game function is to play multiple Rock, Paper, Scissors games.
+Best of 5 rules, if nobody has won 3 times in 5 games, its a draw.
+Code currently doesn't look for situations where draw is inevitable.
 */
 
-let rps = ["Rock", "Paper", "Scissors"]
-
+const rps = ["Rock", "Paper", "Scissors"]
 let getComputerChoice = () => rps[Math.floor(Math.random() * rps.length)];
+let playerScore = 0;
+let computerScore = 0;
+let round = 0;
 
-function playRps(playerSelection, computerSelection) {
+function playRps(playerSelection, computerSelection) {   
+    round++
     playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
     if ((playerSelection === "Rock" && computerSelection === "Scissors") || 
     (playerSelection === "Scissors" && computerSelection === "Paper") || 
     (playerSelection === "Paper" && computerSelection === "Rock")) {
-        console.log("You win!");
+        console.log("Success!");
+        return playerScore++;
+    
     }
     else if (playerSelection === computerSelection){
         console.log("Draw.");
+
     }
     else {
-        console.log("You lose!");
+        console.log("Failure");
+        return computerScore++;
+    }
+    
+}
+
+function isGameComplete() {
+    if (playerScore === 3 || computerScore === 3 || round === 5){
+        gameComplete();
     }
 }
 
-const playerSelection = prompt("Rock, Paper or Scissors?");
-const computerSelection = getComputerChoice();
+function gameComplete () {
+    if (playerScore > computerScore) {
+        console.log("Winner!");
+    }
+    else if (playerScore < computerScore) {
+        console.log("Loser...");
+    }
+    else {
+        console.log("No victor has been determined.");
+    }
+}
 
-playRps(playerSelection, computerSelection);
 
-// function game() {
-//     for (let i=0; i<5; i++){
-//         playRps(playerSelection, computerSelection);
-//     }
-// }
-// game();
+function game() {
+    for (let i=0; i<5; i++) {
+        let playerSelection = prompt("Rock, Paper or Scissors?");
+        let computerSelection = getComputerChoice();
+        playRps(playerSelection, computerSelection);
+        isGameComplete();
+        if (playerScore === 3 || computerScore === 3 || round ===5){
+            break;
+        }
+    }
+}
+game();
